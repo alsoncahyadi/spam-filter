2c896f27383f898ba6d9b212ca6f57ff64205e1b
alsoncahyadi <alson.cahyadi@gmail.com>
2017-09-07 16:04:24 +0700

    added to_dataset

--- spam.py
+++ spam.py
@@ -1,8 +1,22 @@
-from nltk import tokenize
+from nltk import tokenize, pos_tag
 import sklearn
 import glob
 
 ENRON_PATH = "enron3/"
+
+def to_features(word):
+	features = {
+		'word': word,
+		'POS tag': word,
+	}
+	return features
+
+def to_dataset(raw_email, is_spam):
+	tokens = tokenize.word_tokenize(raw_email)
+	pos_tagged_tokens = pos_tag(tokens)
+	for pos_tagged_token in pos_tagged_tokens:
+		pos_tagged_token = pos_tagged_token + (is_spam,)
+	return pos_tagged_tokens, is_spam
 
 if __name__ == "__main__":
 	# Read Enron dataset
@@ -10,10 +24,14 @@ if __name__ == "__main__":
 	
 	for filename in glob.glob(ENRON_PATH + "ham/*.txt"):
 		with open(filename, encoding="ISO-8859-1") as fin:
-			dataset.append((tokenize.sent_tokenize(fin.read()), 0))
+			dataset.append(to_dataset(fin.read(), 0))
 
 	for filename in glob.glob(ENRON_PATH + "spam/*.txt"):
+		print(filename)
 		with open(filename, encoding="ISO-8859-1") as fin:
-			dataset.append((tokenize.sent_tokenize(fin.read()), 1))
+			dataset.append(to_dataset(fin.read(), 1))
 
 	print(len(dataset))
+	X, y = dataset[0]
+	print(X)
+	print(y)
